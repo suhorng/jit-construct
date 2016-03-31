5d8f2ebf34e82c6fea7c51f3f1296a78755d5a0f
suhorng <suhorng@gmail.com>
2016-03-31 17:03:51 +0800

    Some update

--- compiler-opt/CodegenX86.hs
+++ compiler-opt/CodegenX86.hs
@@ -1,11 +1,14 @@
-{-# LANGUAGE FlexibleContexts, DeriveGeneric #-}
+{-# LANGUAGE FlexibleContexts, DeriveGeneric, RecursiveDo #-}
 
 module CodegenX86 where
 
 import CoreExpr
 
+import qualified Data.Set as S
+
 import Control.Monad.State.Strict
 import Control.Monad.Writer
+import Control.Monad.Fix
 import Control.Monad
 
 import Text.PrettyPrint.GenericPretty
@@ -70,28 +73,20 @@ genX86bf e = concat $ map (++ "\n") . execWriter . evalStateT genCode $ 0 where
           "  pop edi",
           "  ret"]
 
-newLabel :: (MonadState Int m) => m String
-newLabel = do
-  lbl <- get
-  modify (+1)
-  return ("L" ++ show lbl)
-
 genX86bf' :: (MonadState Int m, MonadWriter [String] m) => VX86Inst -> m ()
 genX86bf' (MOV dst src) = tells ["  mov   ", extractOp dst, ", ", extractOp src]
 genX86bf' (ADD dst src) = tells ["  add   ", extractOp dst, ", ", extractOp src]
 genX86bf' (PUSH src) =    tells ["  push  ", extractOp src]
 genX86bf' (POP src) =     tells ["  pop   ", extractOp src]
 genX86bf' (CALL fn) =     tells ["  call  ", fn]
-genX86bf' (WhileNZ ptr loop) = do
-  let ptr' = case ptr of { Reg reg -> Indir reg 0; Val n -> Indir "" n }
-  lbl1 <- newLabel
-  lbl2 <- newLabel
-  tells [lbl1, ":"]
-  tells ["  cmp   ", extractOp ptr', ", ", extractOp (Val 0)]
-  tells ["  jz    ", lbl2]
-  mapM_ genX86bf' loop
-  tells ["  jmp   ", lbl1]
-  tells [lbl2, ":\n"]
+genX86bf' (JMP lbl) =     tells ["  jmp   ", lbl]
+genX86bf' (LABEL lbl) =   tells [lbl, ":"]
+genX86bf' (Ifz (Reg reg) lbl) = do
+  tells ["  or    ", reg, ", ", reg]
+  tells ["  jz    ", lbl]
+genX86bf' (Ifz src@(Indir _ _) lbl) = do
+  tells ["  test  byte ", extractOp src, ", 0"]
+  tells ["  jz    ", lbl]
 genX86bf' inst = error $ "genX86bf': unrecognized instruction '" ++ show inst ++ "'"
 
 extractOp (Reg reg) = reg
@@ -109,15 +104,18 @@ data VX86Inst =
   | PUSH VX86Op
   | POP VX86Op
   | CALL String
+  | JMP String
+  | LABEL String
+  | Ifz VX86Op String
   | AddNew Int VX86Op VX86Op
-  | Spill Int Int -- save a virtual register to memory
+  | Kill VX86Op (Maybe VX86Op) -- save a virtual register to memory
   | Cache VX86Op VX86Op -- cache a value in a register; hence can be discarded
   | WhileNZ VX86Op VX86 -- while the content of the register is not zero
   | Call String VX86Op
   deriving (Generic, Show)
 
-data VX86Op = Virt Int | Arg Int | Local Int
-            | Reg String  | Val Int | MemImm Int | MemVirt Int
+data VX86Op = Virt Int | Arg Int | Local Int | MemImm Int | MemVirt Int
+            | Reg String  | Val Int
             | Indir String Int
             deriving (Generic, Show, Eq)
 
@@ -135,16 +133,57 @@ injVX86 (Store (Imm n) (Imm m) e) = MOV (MemImm n) (Val m):injVX86 e
 injVX86 (Store (Imm n) (Var y) e) = MOV (MemImm n) (Virt y):injVX86 e
 injVX86 (While x (x1, x2) e e')
   | x1 == x2 =
-      WhileNZ (Virt x1)
+      WhileNZ (MemVirt x1)
         (injVX86 e):
       Cache (Virt x) (Virt x1):
       injVX86 e'
   | otherwise =
       Cache (Virt x) (Virt x1):
-      WhileNZ (Virt x)
+      WhileNZ (MemVirt x)
         (injVX86 e ++
         [MOV (Virt x) (Virt x2)]):
       injVX86 e'
 injVX86 (GetChar x e) = Call "_rt_getchar" (Virt x):injVX86 e
 injVX86 (PutChar x e) = Call "_rt_putchar" (Virt x):injVX86 e
 injVX86 Stop = []
+
+newLabel :: (MonadState Int m) => m String
+newLabel = do
+  lbl <- get
+  modify (+1)
+  return ("L" ++ show lbl)
+
+remWhile :: VX86 -> VX86
+remWhile insts = execWriter $ evalStateT (mapM remWhile' insts) 0
+
+remWhile' :: (MonadState Int m, MonadWriter VX86 m, MonadFix m) => VX86Inst -> m ()
+remWhile' (WhileNZ cond loop) = mdo
+  lbl1 <- newLabel
+  tell [ LABEL lbl1
+       , Ifz cond lbl2 ]
+  mapM_ remWhile' loop
+  lbl2 <- newLabel
+  tell [ JMP lbl1
+       , LABEL lbl2 ]
+remWhile' insts = tell [insts]
+
+remKill :: VX86 -> VX86
+remKill = concatMap remKill'
+
+remKill' :: VX86Inst -> [VX86Inst]
+remKill' (Kill src Nothing) = []
+remKill' (Kill src (Just dst@(Local _))) = [MOV dst src]
+remKill' (WhileNZ cond loop) = [WhileNZ cond (remKill loop)]
+remKill' inst@(Kill src (Just dst)) = error $ "remKill': unsupported kill dst: '" ++ show inst ++ "'"
+remKill' inst = [inst]
+
+crossOps (AddNew n op1 op2:insts) = appendIfCross (Virt n) insts (crossOps insts)
+crossOps (Cache (Virt n) op:insts) = appendIfCross (Virt n) insts (crossOps insts)
+crossOps (WhileNZ op1 insts':insts) = crossOps insts' ++ crossOps insts
+
+renameOps [] = []
+renameOps (inst:insts) = renameOps insts
+
+remPseudoInst :: VX86 -> VX86
+remPseudoInst = undefined
+
