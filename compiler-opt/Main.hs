b45298065027ee0c57e09e0a5d6de840ada75e46
suhorng <suhorng@gmail.com>
2016-05-04 22:20:46 +0800

    Update timing information in Main

--- compiler-opt/Main.hs
+++ compiler-opt/Main.hs
@@ -31,15 +31,19 @@ testTime fn = do
   let !ir''' = peval ir''
   hPutStr stderr . show . (/ 10^12) . fromIntegral =<< getCPUTime; hPutStrLn stderr " bindeval"
   let !ir'''' = bindeval ir'''
-  hPutStr stderr . show . (/ 10^12) . fromIntegral =<< getCPUTime; hPutStrLn stderr " done"
-  --print ir''''
-  --putStr $ printCode ir''''
-  let inj = injVX86 ir''''
-      kld = insertKill inj
-      lmd = limitActiveVars kld
-      kld2 = insertKill lmd
-      col = collapse kld2
-      asm = genCode col
-  putStr $ CodegenX86.printCode asm--putStrLn (genCCode col)
+  hPutStr stderr . show . (/ 10^12) . fromIntegral =<< getCPUTime; hPutStrLn stderr " injx86"
+  let !inj = injVX86 ir''''
+  hPutStr stderr . show . (/ 10^12) . fromIntegral =<< getCPUTime; hPutStrLn stderr " liveness"
+  let !kld = insertKill inj
+  hPutStr stderr . show . (/ 10^12) . fromIntegral =<< getCPUTime; hPutStrLn stderr " spill"
+  let !lmd = limitActiveVars kld
+  hPutStr stderr . show . (/ 10^12) . fromIntegral =<< getCPUTime; hPutStrLn stderr " liveness"
+  let !kld2 = insertKill lmd
+  hPutStr stderr . show . (/ 10^12) . fromIntegral =<< getCPUTime; hPutStrLn stderr " assign"
+  let !col = collapse kld2
+  hPutStr stderr . show . (/ 10^12) . fromIntegral =<< getCPUTime; hPutStrLn stderr " gencode"
+  let !asm = genCode col
+  hPutStr stderr . show . (/ 10^12) . fromIntegral =<< getCPUTime; hPutStrLn stderr " print"
+  putStr $ CodegenX86.printCode asm
 
 main = getArgs >>= testTime . head
